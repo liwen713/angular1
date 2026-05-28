ae08978182229e49ba8777ffe62a3680d9a5a140
Liwen Albrecht <albrechtliwen1@gmail.com>
2026-05-28 14:32:50 -0300

    Se movieron las carpetas about, contact, home y projects a la carpeta vistas

--- mi-portfolio/src/app/app.routes.ts
+++ mi-portfolio/src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { Home } from './home/home';
-import { About } from './about/about';
-import { ProjectsComponent } from './projects/projects';
-import { Contact } from './contact/contact';
+import { Home } from './vistas/home/home';
+import { About } from './vistas/about/about';
+import { ProjectsComponent } from './vistas/projects/projects';
+import { Contact } from './vistas/contact/contact';
 import { NavComponent } from './shared/nav/nav';
 import { FooterComponent } from './shared/footer/footer';
 
