7c469bb5ea1e48e9ac51184b4ea78174329bfac2
Liwen Albrecht <albrechtliwen1@gmail.com>
2026-06-11 14:44:57 -0300

    Se realizaron cambios referidos a la clase del 11/06

--- mi-portfolio/src/app/vistas/projects/projects.ts
+++ mi-portfolio/src/app/vistas/projects/projects.ts
@@ -14,11 +14,17 @@ export class ProjectsComponent {
   projects: Project[] = [];
   isLoading: boolean = true;
   errorMessage: string = '';
+  goToGithub: any;
+
+  projectsList: any;
 
   constructor(private projectService: ProjectService) 
   { 
     this.projectService.getProjects().subscribe({
-    next: (data) => console.log(data),
+    next: (data) => {
+      this.projectsList = data;
+      console.log(data);
+    },
     error: (error) => console.error(error),
     complete: () => console.info('complete') 
     })
