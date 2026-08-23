8ee3bde262b46e0bd6637b7379d840e3d1ceb157
Liwen Albrecht <albrechtliwen1@gmail.com>
2026-08-23 20:35:20 -0300

    Se actualizaron las rutas de app.routes

--- mi-portfolio/src/app/app.routes.ts
+++ mi-portfolio/src/app/app.routes.ts
@@ -5,11 +5,13 @@ import { About } from './vistas/about/about';
 import { ProjectsComponent } from './vistas/projects/projects';
 import { Contact } from './vistas/contact/contact';
 import { Registro } from './vistas/registro/registro';
+import { ProjectForm } from './vistas/project-form/project-form';
 
 export const routes: Routes = [
   { path: 'home', component: Home },
   { path: 'about', component: About },
   { path: 'projects', component: ProjectsComponent },
+  { path: 'project-form', component: ProjectForm },
   { path: 'contact', component: Contact },
   { path: 'registro', component: Registro },
   { path: '', redirectTo: '/home', pathMatch: 'full' }
